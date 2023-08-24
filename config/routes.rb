00676b64c84cc3cdd137e0143ae47d38e68fa77c
Reyna May <roma.reynamayA@gmail.com>
2023-08-23 19:58:23 -0600

    Implement the About page and display it to the navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   root to: 'products#index'
-
+  
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
   end
-
+  get 'about', to: 'about#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
