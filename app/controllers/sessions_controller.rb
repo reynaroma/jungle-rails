6758b6133bb0358aa53284f789fd7eac894b7508
Reyna May <roma.reynamayA@gmail.com>
2023-08-29 13:19:29 -0600

    Modify the authenticate method to authenticate properly

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,16 @@
 class SessionsController < ApplicationController
   skip_before_action :require_user, only: [:new, :create]
   def new
-    
+
   end
   
   def create
-    user = User.find_by(email: params[:session][:email])
-    if user && user.authenticate_with_credentials(params[:session][:password])
+    user = User.authenticate_with_credentials(params[:session][:email], params[:session][:password])
+    if user
       session[:user_id] = user.id
       redirect_to products_path, notice: 'Logged in!'
     else
-      Rails.logger.error("Login failed for email: #{params[:session][:email]}")
+      Rails.logger.error("Login failed for email: #{params[:session][:password]}")
       redirect_to login_path, alert: 'Email or password is incorrect'
     end
   end
